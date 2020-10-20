226a3b30802729c53d7780168f5c198943ff201a
harshaldhone <harshaldhone99@gmail.com>
2020-10-20 16:54:55 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+const { app, BrowserWindow } = require('electron')
+const url = require("url");
+const path = require("path");
+
+let win;
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 600, 
+    height: 600,
+    backgroundColor: '#FFFFFF',
+    icon: `src/favicon.ico`
+  })
+
+
+  //win.loadURL(`file://${__dirname}/dist/index.html`)
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/dist/wiro/index.html`),
+      protocol: "file:",
+      slashes: true
+    })
+);
+
+
+
+  //// uncomment below to open the DevTools.
+  // win.webContents.openDevTools()
+
+  // Event when the window is closed.
+  win.on('closed', function () {
+    win = null
+  })
+}
+
+// Create window on electron intialization
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+
+  // On macOS specific close process
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // macOS specific close process
+  if (win === null) {
+    createWindow()
+  }
+})
